949d613057c27d6ccc5885ad08a1ef87b14d2fda
samuelestacio <jsholandasoares@gmail.com>
2025-08-30 22:19:58 +0000

    trabalho xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,24 @@
+#include <stdio.h>
+
 int main() {
-    // Mover a torre 5 casas para a direita 
+    // Bispo: 5 casas na diagonal superior direita
+    printf("Movimento do Bispo:\n");
     for (int i = 0; i < 5; i++) {
-    printf ("Direita\n"); // imprime a direçâo do movimento 
+        printf("Cima\n");
+        printf("Direita\n");
     }
+
+    // Torre: 5 casas para a direita
+    printf("\nMovimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+
+    // Rainha: 8 casas para a esquerda
+    printf("\nMovimento da Rainha:\n");
+    for (int i = 0; i < 8; i++) {
+        printf("Esquerda\n");
+    }
+
     return 0;
 }
